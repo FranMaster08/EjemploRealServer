1a8617bfd44b283238a917c4823daa10d6fff816
Francisco Jimenez <franjicohen@gmail.com>
2021-02-24 20:28:36 -0300

    Se crea servidor base

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const path = require('path');
+const app = express()
+process.env.PORT = process.env.PORT || 5000
+app.get('/', (req, res) => {
+    res.status(200).json({ mensaje: 'ok' })
+});
+app.listen(process.env.PORT, (err) => {
+    let mensaje = `Ocurrio un error iniciando Servidor en el puerto :${process.env.PORT} :: error: err`
+    if (!err) {
+        mensaje = `Servidor Corriendo en el puerto :${process.env.PORT}`
+    }
+    console.log(mensaje);
+})
